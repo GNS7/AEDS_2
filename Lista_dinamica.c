77203f6e33809943df35b36331ba913420413140
GNS7 <61403819+GNS7@users.noreply.github.com>
2022-12-22 19:36:28 -0300

    Update Lista_dinamica.c
    Adiciona Inserir e Remover por posição.

--- Lista_dinamica.c
+++ Lista_dinamica.c
@@ -51,8 +51,8 @@ int main()
                 printf(
                     "\n1. Colocar na lista\n"              // emlista um novo numero"
                     "2. Colocar na lista em uma posicao\n" // emlista um novo numero em uma posicao que o usuario escolher
-                    "2. Retirar da lista\n"                // desemlista um numero
-                    "3. Retirar da lista em uma posicao\n" // desemlista um numero em uma posicao que o usuario escolher
+                    "3. Retirar da lista\n"                // desemlista um numero
+                    "4. Retirar da lista em uma posicao\n" // desemlista um numero em uma posicao que o usuario escolher
                     "3. Conferir se a lista esta vazia\n"  //
                     "4. Conferir se a lista esta cheia\n"  // N funciona
                     "5. Liberar a lista e sair\n"          // libera e sai do programa
@@ -88,6 +88,34 @@ int main()
 
                 case 2:
 
+                    printf("Digite o valor que quer colocar na lista: \n");
+
+                    scanf("%d", &temp);
+
+                    printf("Digite a posicao que quer colocar na lista: \n");
+
+                    scanf("%d", &posicao);
+
+                    int inserirPosicao = inserirItemPosicao(lista, temp, posicao);
+
+                    if (inserirPosicao == 0)
+                    {
+                        printf("A lista esta cheia\n");
+                    }
+                    else if (inserirPosicao == 1)
+                    {
+                        printf("Numero adicionado: %i\n", temp);
+                        imprimeLista(lista);
+                    }
+                    else
+                    {
+                        printf("Error");
+                    }
+                    break;
+
+                case 3:
+                    printf(""); // da erro se declarar int antes do printf
+
                     int remover = removerItem(lista, &descarte);
 
                     if (remover == 0)
@@ -95,6 +123,27 @@ int main()
                         printf("A lista esta vazia\n");
                     }
                     else if (remover == 1)
+                    {
+                        printf("Numero %d retirado\n", descarte);
+                        imprimeLista(lista);
+                    }
+                    else
+                    {
+                        printf("Error");
+                    }
+                    break;
+                case 4:
+                    printf("Digite a posicao que quer retirar da lista: \n");
+
+                    scanf("%d", &posicao);
+
+                    int removerPosicao = removerItemPosicao(lista, &descarte, posicao);
+
+                    if (removerPosicao == 0)
+                    {
+                        printf("A lista esta vazia\n");
+                    }
+                    else if (removerPosicao == 1)
                     {
                         printf("Numero %d retirado\n", descarte);
                         imprimeLista(lista);
@@ -159,14 +208,13 @@ int inserirItem(Lista *l, int valor)
             aux = aux->proximo;
         aux->proximo = novo;
     }
-    free(aux);
-    free(novo);
     return 1;
 }
 
 int inserirItemPosicao(Lista *l, int valor, int posicao) ////////////////////////N Funciona
 {
     Elemento *novo, *aux, *ant;
+    int count = 0;
 
     novo = (Elemento *)malloc(sizeof(Elemento));
 
@@ -175,10 +223,26 @@ int inserirItemPosicao(Lista *l, int valor, int posicao) ///////////////////////
 
     novo->valor = valor;
     novo->proximo = NULL;
-    if (l->inicio == NULL)
+    if (posicao == 1)
+    {
+        novo->proximo = l->inicio->proximo;
         l->inicio = novo;
+    }
     else
     {
+        aux = l->inicio;
+        while (aux->proximo != NULL) // percorre a lista até o final
+        {
+            if (count == posicao - 2) // se a posicao for igual a posicao que o usuario escolheu4
+            {
+                novo->proximo = aux->proximo;
+                aux->proximo = novo;
+                return 1;
+            }
+            aux = aux->proximo;
+            count++;
+        }
+        aux->proximo = novo; // se a posicao for maior que o tamanho da lista, adiciona na ultima posicao
     }
     return 1;
 }
@@ -196,35 +260,55 @@ int removerItem(Lista *l, int *valor)
         ant = aux;
         aux = aux->proximo;
     }
-    valor = ant->proximo->valor;
-    printf("Valor: %d", valor);
+    *valor = ant->proximo->valor;
     ant->proximo = NULL; // ant->proximo = aux = ultimo elemento da fila
 
     free(aux);
-    free(ant);
     return 1;
 }
 
 int removerItemPosicao(Lista *l, int *valor, int posicao)
 {
     Elemento *aux, *ant;
-    int i;
+    int i, count = 0, count_posicao = 0;
 
     if (l->inicio == NULL) // verifica se a lista esta vazia
         return 0;
 
     aux = l->inicio;
-    for (i = 0; i < posicao; i++)
+    while (aux->proximo != NULL) // percorre a lista até o final
     {
-        ant = aux;
         aux = aux->proximo;
+        count++;
     }
-    if (aux == NULL)
-        return 0;
-    if (aux == l->inicio)
+    if (posicao > count + 1) // verifica se a posicao é maior que o tamanho da lista
+    {
+        printf("Posicao invalida");
+        return -1;
+    }
+    aux = l->inicio;  // RESET aux = primeiro elemento da lista
+    if (posicao == 1) // se a posicao for igual a 1, remove o primeiro elemento
+    {
+        *valor = l->inicio->valor;
         l->inicio = aux->proximo;
+        return 1;
+    }
     else
-        ant->proximo = aux->proximo;
+    {
+        while (aux->proximo != NULL) // percorre a lista até o final
+        {
+            if (count_posicao == posicao - 2) // se a posicao for igual a posicao que o usuario escolheu4
+            {
+                *valor = aux->proximo->valor;
+                aux->proximo = aux->proximo->proximo;
+                return 1;
+            }
+            aux = aux->proximo;
+            count_posicao++;
+            printf("%d", count_posicao);
+        }
+    }
+
     free(aux);
     return 1;
 }
